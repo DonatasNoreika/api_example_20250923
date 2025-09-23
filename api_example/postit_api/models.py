46e28669bcdd0b8003b62ad87df37fb040b89419
DonatasNoreika <donoras@gmail.com>
2025-09-23 18:21:51 +0300

    Initial commit

---
+++ api_example/postit_api/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField()
+    body = models.TextField()
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-created']
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(to="Post", on_delete=models.CASCADE)
+    body = models.TextField()
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body
+
+    class Meta:
+        ordering = ['-created']
+
+
+class PostLike(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    post = models.ForeignKey(to="Post", on_delete=models.CASCADE)
+
+
+class CommentLike(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(to="Comment", on_delete=models.CASCADE)
