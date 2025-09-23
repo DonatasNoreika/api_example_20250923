556ab4906d21005fb951a4456724d700c285d82e
DonatasNoreika <donoras@gmail.com>
2025-09-23 18:51:51 +0300

    Padaryta, kad postus leistų kurti tik prisijungusiems

--- api_example/postit_api/views.py
+++ api_example/postit_api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .serializers import PostSerializer
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Post, Comment, PostLike, CommentLike
 
 # Create your views here.
@@ -8,3 +8,7 @@ from .models import Post, Comment, PostLike, CommentLike
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
