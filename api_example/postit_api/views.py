7d85d138de6f0b0950d189e2791acab9129928a9
DonatasNoreika <donoras@gmail.com>
2025-09-24 18:23:19 +0300

    Padaryta, kad postus leistų peržiūrėti (po vieną), redaguoti, trinti tik įrašo autoriui

--- api_example/postit_api/views.py
+++ api_example/postit_api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .serializers import PostSerializer
 from rest_framework import generics, permissions
 from .models import Post, Comment, PostLike, CommentLike
+from rest_framework.exceptions import ValidationError
 
 # Create your views here.
 
@@ -18,4 +19,21 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def delete(self, request, *args, **kwargs):
+        post = Post.objects.filter(pk=kwargs['pk'], user=self.request.user)
+        if post.exists():
+            return self.destroy(request, *args, **kwargs)
+        else:
+            raise ValidationError("Negalima trinti svetimų pranešimų")
+
+    def put(self, request, *args, **kwargs):
+        post = Post.objects.filter(pk=kwargs['pk'], user=self.request.user)
+        if post.exists():
+            return self.update(request, *args, **kwargs)
+        else:
+            raise ValidationError("Negalima redaguoti svetimų pranešimų")
+
+
+
 
